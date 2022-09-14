3237f5d76a5675ba7a048c1e672be9055a748109
Mateo Sheshi <sheshi.mateo01@gmail.com>
2022-09-14 12:04:02 +0200

    chore: added form helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
@@ -14,6 +15,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
